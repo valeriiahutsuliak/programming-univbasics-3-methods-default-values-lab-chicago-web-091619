efb8b1aec90cc06b223ba315b6de2b33ae048394
Valerria Hutsuliak <valeria.gutsuliak@gmail.com>
2019-10-03 17:54:29 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,4 @@
 # Your code here
-lib/meal_choice.RUBY_VERSION
-def meal_choise(veg1,veed2)
-puts "What a nutritions meal!"
-puts " A plate of #{protein}with #{veg1}and {veg2}."
+def meal_choice(meal ="meat")
+  meal
+end 
